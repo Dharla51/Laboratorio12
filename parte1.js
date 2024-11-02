1ad44494d89754e14a66af801aba014e92dc79f5
Dharla51 <dharlajduran@unicesar.edu.co>
2024-11-01 20:43:51 -0500

    LISTO

--- parte1.js
+++ parte1.js
@@ -1,1 +1,27 @@
+let a= prompt("Digite el primer numero: "); 
+let b= prompt("Digite el segundo numero: ");
+ 
+function suma(a,b){
+    return a+b; 
 
+}
+function resta(a,b){
+    return a-b;
+    
+}
+function division(a,b){
+    return a/b;
+}
+function multiplicacion(a,b){
+    return a*b;
+}
+
+
+console.log("La suma es: "+suma(a,b)); 
+console.log("La resta es: "+resta(a,b)); 
+console.log("La division es: "+division(a,b)); 
+console.log("La multiplicacion es: "+multiplicacion(a,b)); 
+document.write("La suma es: "+suma(a,b)+"<br>"); 
+document.write("La resta es: "+resta(a,b)+"<br>"); 
+document.write("La division es: "+division(a,b)+"<br>"); 
+document.write("La multiplicacion es: "+multiplicacion(a,b)+"<br>"); 
--- parte2.js
+++ parte2.js
@@ -1,1 +1,32 @@
+let a= parseInt(prompt("Digite el primer numero: ")); 
+let b= parseInt(prompt("Digite el segundo numero: "));
+let fun=prompt("Digite la operacion a realizar (suma 1 /resta 2 /multiplicacion 3 /division 4): "); 
+ 
+function suma(a,b){
+    return a+b; 
+}
+
+function resta(a,b){
+    return a-b;
+}
+
+function division(a,b){
+    return a/b;
+}
+
+function multiplicacion(a,b){
+    return a*b;
+}
+
+function calcular(a, b, fun) { 
+    switch (fun) { 
+        case "1": return suma(a, b); 
+        case "2": return resta(a, b); 
+        case "3": return multiplicacion(a, b); 
+        case "4": return division(a, b); 
+        default: return "OPERADOR INVALIDO"; } 
+}
+
+console.log("el resultado es: "+calcular(a,b,fun)); 
+document.write("el resultado es: "+calcular(a,b,fun)+"<br>"); 
 
